8088d0a26945236738887058fa89d51a9539c130
Sharjeel <ahmadsharjeel3@gmail.com>
2020-09-19 18:50:38 +1000

    Update colors

--- src/ui-library/theme/colors.js
+++ src/ui-library/theme/colors.js
@@ -2,7 +2,9 @@ export const brandColors = {
   green: '#00a843',
   grey: '#2A3842',
   mediumGrey: '#4a4a4a',
+  blueGrey: '#455e70',
   lightGrey: 'rgba(42, 56, 66, 0.6)',
   white: '#ffffff',
-  black: '#000000'
+  black: '#000000',
+  blueviolet: 'blueviolet'
 };
