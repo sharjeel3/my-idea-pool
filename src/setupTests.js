95701ae9aed72503fd60700ec1de7e65bedd6f2a
Sharjeel <ahmadsharjeel3@gmail.com>
2020-09-20 16:32:52 +1000

    Setup spy for local storage

--- src/setupTests.js
+++ src/setupTests.js
@@ -6,3 +6,13 @@ import 'jest-enzyme';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
+
+// Local Storage
+Object.defineProperty(window, 'localStorage', {
+  value: {
+    getItem: jest.fn(() => null),
+    setItem: jest.fn(() => null),
+    removeItem: jest.fn(() => null)
+  },
+  writable: true
+});
